04970934d3ec0d2b34db72c2d9632a4a391199d3
Ohkthx <o4kthx@gmail.com>
2020-12-28 04:21:57 -0600

    Added documentation

--- photoco.py
+++ photoco.py
@@ -5,7 +5,9 @@
 This program checks and updates to new version of PaperMC if it is
 available for download.
 
-usage: photoco.py [-h] (--use | --create) [-s] config
+usage: photoco.py [-h] -v version paper_jar
+version:   Major version for Minecraft, ie: 1.16
+paper_jar: paper.jar / paperclip.jar / Paper-<VERSION>.jar file location.
 """
 
 import argparse
@@ -22,6 +24,7 @@ HTML_CLASS = "md5sum"
 
 
 class ArgumentError(Exception):
+    """Raised if an invalid argument was attempted to be utilized."""
     pass
 
 
@@ -50,12 +53,20 @@ def main():
 
 
 def generate_url(version, fingerprint=False):
+    """Creates a URL to either download an updated PaperMC server or
+    download a 'fingerprint'.
+
+    A 'fingerprint' URL is later parsed for a md5sum.
+    """
     if not version.strip():
         raise ArgumentError("Invalid 'version' argument provided.")
 
+    # Add the supplied version to the URL.
     base_url = ("https://papermc.io/ci/job/Paper-"
                 f"{version.strip()}"
                 "/lastStableBuild/artifact/paperclip.jar")
+
+    # If we need the 'fingerprint', add the extension.
     if fingerprint:
         return f"{base_url}/*fingerprint*/"
 
@@ -63,17 +74,20 @@ def generate_url(version, fingerprint=False):
 
 
 def get_local_md5(paper_file):
+    """Check the md5sum of the locally provided PaperMC installation."""
     fname = paper_file.strip()
     if not fname or not Path(fname).is_file():
         raise ArgumentError("Bad 'paper.jar' provided, could not generate"
                             " md5sum.")
 
+    # Generate the md5sum
     with open(fname, 'rb') as f:
         data = f.read()
         return hashlib.md5(data).hexdigest()
 
 
 def get_remote_md5(url, htmlclass):
+    """Check the most current md5sum by parsing a webpage."""
     if not url:
         raise ArgumentError("Bad URL provided.")
     if not htmlclass:
@@ -81,6 +95,7 @@ def get_remote_md5(url, htmlclass):
 
     req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
 
+    # Make our connection to the remote web page.
     try:
         webpage = urlopen(req)
         soup = BeautifulSoup(webpage, features="lxml")
@@ -88,15 +103,19 @@ def get_remote_md5(url, htmlclass):
     except Exception as exc:
         raise exc
 
+    # Validate we successfully parsed some information.
     if not md5str:
         return Exception("md5sum returned from URL was invalid.")
 
+    # At the time of creation, returned string from parsing is similar to:
+    #  'MD5: 4abf78f9f2381858db7b5efe3cfa1b3a'
     md5split = md5str.split()
     if len(md5split) == 2 and len(md5split[1]) == 32:
         return md5split[1]
     elif len(md5split[0]) != 32:
         return Exception("md5sum returned from URL was invalid.")
 
+    # If PaperMC changes format and pos 0 in array is 32 characters long.
     return md5split[0]
 
 
